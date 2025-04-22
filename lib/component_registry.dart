f02f17a063c57208e86377fa37ad73cc5c521782
sporadic monster <sporadicmonster@Mac-mini-sporadic.local>
2025-04-22 17:18:31 +0300

    to deploy

--- lib/component_registry.dart
+++ lib/component_registry.dart
@@ -4,6 +4,6 @@ import 'package:wtflutter/wtflutter.dart';
 // Map of component names to their respective pages
 final Map<String, Widget Function()> componentRegistry = {
   'login_form': () => const LoginFormPage(),
-  'signup_form': () => const SignupFormPage(),
+  //'signup_form': () => const SignupFormPage(),
   // Add other components here...
 };
